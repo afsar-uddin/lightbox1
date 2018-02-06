1370d30d7871c3f4ca1af7715aed6855456a537f
Md Afsar Uddin <afsarwebdev@gmail.com>
2018-02-07 00:30:39 +0600

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+// variables
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+
+// add image to overlay
+$overlay.append($image);
+// add overlay
+$("body").append($overlay);
+// Capture the click event on a link to an image
+
+$("#imageGallery a").click(function(event){
+	event.preventDefault();
+	var imageLocation = $(this).attr("href");
+	$overlay.show();
+
+	// Update overlay with the image linked in the link
+	$image.attr("src", imageLocation);
+	// console.log(href);
+});
+
+$overlay.click(function(){
+	$overlay.hide();
+});
+
+ 
